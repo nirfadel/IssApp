038603ba9d13e368a08291776fce12b4a63e732e
nirfadel <32796819+nirfadel@users.noreply.github.com>
2022-10-10 08:57:51 +0300

    removing unnecessary calls

--- src/app/iss-display/app.iss.dialog.ts
+++ src/app/iss-display/app.iss.dialog.ts
@@ -1,17 +1,13 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import {NgbModal, ModalDismissReasons, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'iss-dialog',
   templateUrl: './app.iss.dialog.html'
 })
 export class IssdModalComponent {
-  closeResult = '';
-
   @Input() public issModel;
-
-  @Output() passEntry: EventEmitter<any> = new EventEmitter();
 
   constructor(public activeModal: NgbActiveModal) {}
 
